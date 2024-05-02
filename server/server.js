8a02d8c181c22280377b75a612bb1713fc1d7483
Axel Gil <axelgil60@gmail.com>
2024-05-02 17:25:34 +0200

    added template

--- server/server.js
+++ server/server.js
@@ -1,5 +1,16 @@
-const app = require('./app')
-const port = process.env.PORT || '3000'
-app.listen(port, () =>
-  console.log("Server is listening on port " + port)
+require("dotenv").config();
+const express = require("express");
+const UserRouter = require("./routes/users");
+const SecurityRouter = require("./routes/security");
+const checkAuth = require("./middlewares/checkAuth");
+
+const app = express();
+
+app.use(express.json());
+
+app.use(SecurityRouter);
+app.use("/users", UserRouter);
+
+app.listen(process.env.PORT, () =>
+  console.log("Server listening on port " + process.env.PORT)
 );
