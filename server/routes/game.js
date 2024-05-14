af8c7024ce6db9f57777efa9b21e8c490d40bb51
Axel Gil <axelgil60@gmail.com>
2024-05-14 12:07:00 +0200

    added implementation of hateoas

--- server/routes/game.js
+++ server/routes/game.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
 const gameController = require("../controllers/game");
+const hateoasMiddleware = require("../middlewares/hateoasMiddleware");
 
-router.post("", gameController.post);
-router.get("/:gameId", gameController.iget);
+const gameMovesHateoas = {
+  placeMove: {
+    method: "PATCH",
+    path: "/:gameId/moves",
+  },
+  reset: {
+    method: "PATCH",
+    path: "/:gameId",
+  },
+};
+
+router.post("",  gameController.post);
+router.get("/:gameId", hateoasMiddleware(gameMovesHateoas), gameController.iget);
 router.patch("/:gameId", gameController.patch);
 router.patch('/:gameId/moves', gameController.placeMove);
 
